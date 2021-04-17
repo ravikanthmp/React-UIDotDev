9d4979335c544b2dd081699051f09c9a62188ec6
ravi <ravikanthmp123@gmail.com>
2021-04-16 23:18:21 -0700

    add functional component

--- app/components/popular.js
+++ app/components/popular.js
@@ -1,5 +1,28 @@
 import React from "react";
 import ReactDOM from 'react-dom';
+import PropTypes from 'prop-types';
+
+function LanguageNavBar({langs, updateLanguage, getLanguage}){
+
+    console.log(`this is ${this}`)
+    return (
+        <ul className="flex-center">
+            {langs.map(
+                lang =>
+                    <li key={lang}>
+                        <button className="btn-clear nav-link"
+                                onClick={() => updateLanguage(lang)}
+                                style={getLanguage() === lang ? {color : 'red'} : {color : null}}
+                        >
+                            {lang}
+                        </button>
+                    </li>
+            )
+            }
+        </ul>
+    )
+
+}
 
 export default class Popular extends React.Component{
 
@@ -18,21 +41,23 @@ export default class Popular extends React.Component{
         })
     }
 
-    render() {
-
-        return (
-            <ul className="flex-center">
-                {this.props.languages.map(
-                    lang =>
-                        <li key={lang}>
-                            <button className="btn-clear nav-link" onClick={() => this.updateLanguage(lang)}>
-                                {lang}
-                            </button>
-                        </li>
-                )
-                }
-                <li>{this.state.selectedLanguage}</li>
-            </ul>
-        )
+    getLanguage(){
+        return this.state.selectedLanguage;
     }
+
+    render() {
+        let p = {
+            langs : ['a','b'],
+            updateLanguage : this.updateLanguage
+        }
+       return <LanguageNavBar
+                    langs={['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python'] }
+                    updateLanguage={this.updateLanguage.bind(this)}
+                    getLanguage={this.getLanguage.bind(this)}
+       />
+    }
+}
+
+Popular.propTypes = {
+    languages : PropTypes.arrayOf(PropTypes.string).isRequired
 }
